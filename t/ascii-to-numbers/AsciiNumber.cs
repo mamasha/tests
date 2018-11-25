4518c9d4647832a72fd96cf0c07202a9507b72d6
Reuven Bass <reuven.bass@gmail.com>
2018-11-25 12:50:11 +0200

    execution schema is ready

---
+++ t/ascii-to-numbers/AsciiNumber.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ascii_to_numbers
+{
+    interface IAsciiNumber
+    {
+        bool IsValid { get; }
+        string ParsedNumber { get; }
+        void PushLine(string line);
+    }
+
+    class AsciiNumber : IAsciiNumber
+    {
+        #region members
+
+        private readonly int _noOfDigits;
+        private readonly int _lineNo;
+        private readonly IAsiiDigit[] _digits;
+
+        #endregion
+
+        #region construction
+
+        public static IAsciiNumber New(int noOfDigits, int lineNo)
+        {
+            return
+                new AsciiNumber(noOfDigits, lineNo);
+        }
+
+        private AsciiNumber(int noOfDigits, int lineNo)
+        {
+            _noOfDigits = noOfDigits;
+            _lineNo = lineNo;
+            _digits = new IAsiiDigit[noOfDigits];
+
+            for (int i = 0; i < noOfDigits; i++)
+            {
+                _digits[i] = AsciiDigit.New();
+            }
+        }
+
+        #endregion
+
+        #region interface
+
+        bool IAsciiNumber.IsValid
+        {
+            get
+            {
+                foreach (var digit in _digits)
+                {
+                    if (!digit.IsValid)
+                        return false;
+                }
+
+                return true;
+            }
+        }
+
+        string IAsciiNumber.ParsedNumber
+        {
+            get
+            {
+                var sb = new StringBuilder();
+
+                foreach (var digit in _digits)
+                {
+                    sb.Append(digit.ParsedDigit);
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        void IAsciiNumber.PushLine(string line)
+        {
+            var charCount = 3 * _noOfDigits;
+
+            if (line.Length < charCount)
+                throw new ApplicationException($"Not enough characters ({line.Length}) in line {_lineNo}; should be at least {charCount} characters");
+
+            var next = 0;
+
+            foreach (var digit in _digits)
+            {
+                digit.PushChar(line[next++]);       // a line has groups of three characters each per digit
+                digit.PushChar(line[next++]);
+                digit.PushChar(line[next++]);
+            }
+        }
+
+        #endregion
+    }
+}
--- t/ascii-to-numbers/Program.cs
+++ t/ascii-to-numbers/Program.cs
@@ -1,20 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace ascii_to_numbers
 {
     class Program
     {
-        private static void mainImpl(string[] args)
+        private static void ConvertFile(string rootFolder, string inFile, string outFile)
         {
-            Console.WriteLine("ascii-to-numbers");
+            var inPath = Path.Combine(rootFolder, inFile);
+            var outPath = Path.Combine(rootFolder, outFile);
 
+            var lines = File.ReadAllLines(inPath);      // read them all for now; can be read line line if file is big
+            var lineCount = lines.Length;
+
+            Console.WriteLine($"Processing '{inFile}' ({lineCount} lines) into '{outFile}'");
+
+            if (lineCount % 4 != 0)
+                throw new ApplicationException($"Wrong line count {lineCount} in '{inFile}'; should be modulo of four");
+
+            var numbers = new List<string>();
+
+            for (int next = 0; next < lineCount; )
+            {
+                var ascii = AsciiNumber.New(8, next);   // number of 8 digits at next line no 
+
+                ascii.PushLine(lines[next++]);          // fragment of three line
+                ascii.PushLine(lines[next++]);
+                ascii.PushLine(lines[next++]);
+                next++;                                 // skip empty line
+
+                var number = ascii.ParsedNumber;
+
+                if (ascii.IsValid == false)
+                    number += " INVALID";
+
+                numbers.Add(number);
+            }
+
+            Console.WriteLine($"Parsed {numbers.Count} numbers");
+
+            File.WriteAllLines(outPath, numbers);
+        }
+
+        private static void mainImpl()
+        {
+            var rootFolder = Path.GetFullPath(
+                Directory.GetCurrentDirectory() + "/../..");
+
+            Console.WriteLine($"ascii-to-numbers: root folder is '{rootFolder}'");
+
+            ConvertFile(rootFolder, "input_Q1a.txt", "output_Q1a.txt");
+            ConvertFile(rootFolder, "input_Q1b.txt", "output_Q1b.txt");
         }
 
         static void Main(string[] args)
         {
             try
             {
-                mainImpl(args);
+                mainImpl();
             }
             catch (Exception ex)
             {
