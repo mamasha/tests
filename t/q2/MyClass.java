fd18c3533028e58af62b507b48554e8d0cc2a3d3
Reuven Bass <reuven.bass@gmail.com>
2018-11-28 23:09:20 +0200

    q2 answer comments

--- t/q2/MyClass.java
+++ t/q2/MyClass.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class MyClass
 {
+    // all members can be made final
     private Date m_time;
     private String m_name;
     private List<Long> m_numbers;
@@ -16,8 +17,19 @@ public class MyClass
         m_strings = strings;
     }
 
+    // probably we should override the hashCode() as well
+
     public boolean equals(Object obj) 
     {
+        // probably the class implements a snapshot of some state
+        // if it is true, then the time member should be checked for equality as well
+
+        // https://www.sitepoint.com/implement-javas-equals-method-correctly/
+
+        // method should be made final
+        // method should test for null
+        // method should test for self (optimization)
+
         if (obj instanceof MyClass) {
             return m_name.equals(((MyClass)obj).m_name);
         }
@@ -26,6 +38,9 @@ public class MyClass
     
     public String toString() 
     {
+        // inefficient approach, the execution and memory complexity is O(n*n)
+        // StringBuilder should be used here
+
         String out = m_name;
         for (long item : m_numbers) {
             out += " " + item;
@@ -35,11 +50,25 @@ public class MyClass
     
     public void removeString(String str) 
     {
+        // wrong approach to iteration while collection is being modified
+        // when string is removed from list its size decreases
+        // an OutOfBounds exception here if a string is actually removed
         for (int i = 0; i < m_strings.size(); i++) {
             if (m_strings.get(i).equals(str)) {
                 m_strings.remove(i);
             }
         }
+
+        // a proper solution to remove all strings
+        int i = 0;
+        while (i < m_strings.size()) {
+            if (m_strings.get(i).equals(str)) {
+                m_strings.remove(i);
+                continue;
+            }
+            i++;
+        }
+
     }
 
     public boolean containsNumber(long number) 
@@ -52,6 +81,8 @@ public class MyClass
         return false;
     }
     
+    // inconsistent behaviour here, two consequent calls may return different results
+    // isHistoric() should get a date to compare to from outside
     public boolean isHistoric() {
         return m_time.before(new Date());
     }
